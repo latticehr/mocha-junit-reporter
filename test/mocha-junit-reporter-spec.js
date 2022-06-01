3bdef5dd1e7fa549e219e8f1eedd9763eee0f72b
JP Dillingham <jp.dillingham@lattice.com>
2022-06-01 08:03:35 -0500

    add test to cover dashes and underscores

--- test/mocha-junit-reporter-spec.js
+++ test/mocha-junit-reporter-spec.js
@@ -576,6 +576,25 @@ describe('mocha-junit-reporter', function() {
       });
     });
 
+    it('includes tags and teams containing dashes and underscores from the suite and test names', function(done) {
+      var reporter = createReporter({includeTags: true});
+      var rootSuite = reporter.runner.suite;
+      var suite1 = Suite.create(rootSuite, 'root suite');
+
+      suite1.addTest(createTest('test case has #tag-dash and @team_underscore'));
+
+      runRunner(reporter.runner, function() {
+        if (reporter.runner.dispose) {
+          reporter.runner.dispose();
+        }
+
+        expect(reporter._testsuites[1].testsuite[1].testcase[0]._attr.tags).to.have.lengthOf(2);
+        expect(reporter._testsuites[1].testsuite[1].testcase[0]._attr.tags).to.have.members(['#tag-dash', '@team_underscore']);
+
+        done();
+      });
+    });
+
     it('includes tags and teams from the test config when using Cypress >= ~9.0.0 style _testConfig', function(done) {
       var reporter = createReporter({includeTags: true});
       var rootSuite = reporter.runner.suite;
